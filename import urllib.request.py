8ddc21368290ac0d7e5885594b4366adbb3adbb9
aarondan193 <79936751+aarondan193@users.noreply.github.com>
2023-04-22 16:47:35 -0400

    Add files via upload

---
+++ import urllib.request.py
@@ -0,0 +1,3 @@
+import urllib.request
+page = urllib.request.urlopen('http://services.runescape.com/m=hiscore/ranking?table=0&category_type=0&time_filter=0&date=1519066080774&user=zezima')
+print(page.read())
